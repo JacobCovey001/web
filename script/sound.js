f0e124405714bb8893740dcc0e9319d1004688d8
JacobCovey001 <braydencosmic1234@gmail.com>
2026-03-11 16:42:18 +0000

    Wed, Mar 11, 2026, 9:42 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -1,9 +1,22 @@
 buttons = document.querySelector('.buttons')
+applause = document.querySelector('.applause')
+boo = document.querySelector('.boo')
+tada = document.querySelector('.tada')
+gasp = document.querySelector('.gasp')
+victory = document.querySelector('.victory')
+wrong = document.querySelector('.wrong')
 
-btn = document.createElement('button')
-btn.innerText = "applause"
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+for(let i = 0, i < 6; i = i + 1){
+    btn = document.createElement('button')
+btn.innerText = sounds[i]
 btn.classList.add('btn')
 btn.addEventListener('click', () => {
-    applause.play()
+    .play()
 })
 buttons.appendChild(btn)
+
+
+}
+
